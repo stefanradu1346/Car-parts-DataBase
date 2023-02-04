7ceeb8393cdbd19725695c83d10500e5fe2764ef
Stefan Radu <117312806+stefanradu1346@users.noreply.github.com>
2023-02-04 12:09:42 +0200

    Add files via upload

---
+++ ProjectDB/ProjectDB/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace ProjectDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ShowAll = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Banner", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(585, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 107);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stefan Car Parts";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(573, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(566, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(566, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(697, 237);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 36);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(697, 293);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(307, 32);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(697, 352);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(307, 32);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Insert
+            // 
+            this.Insert.BackColor = System.Drawing.Color.Tan;
+            this.Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insert.Location = new System.Drawing.Point(507, 518);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(138, 59);
+            this.Insert.TabIndex = 7;
+            this.Insert.Text = "INSERT";
+            this.Insert.UseVisualStyleBackColor = false;
+            this.Insert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update.Location = new System.Drawing.Point(651, 518);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(175, 59);
+            this.Update.TabIndex = 8;
+            this.Update.Text = "UPDATE";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.Crimson;
+            this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(832, 518);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(149, 59);
+            this.Delete.TabIndex = 9;
+            this.Delete.Text = "DELETE";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Search
+            // 
+            this.Search.BackColor = System.Drawing.Color.PaleGreen;
+            this.Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search.Location = new System.Drawing.Point(987, 518);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(170, 59);
+            this.Search.TabIndex = 10;
+            this.Search.Text = "SEARCH";
+            this.Search.UseVisualStyleBackColor = false;
+            this.Search.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(237, 678);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.Size = new System.Drawing.Size(1156, 370);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ShowAll
+            // 
+            this.ShowAll.BackColor = System.Drawing.Color.Goldenrod;
+            this.ShowAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowAll.Location = new System.Drawing.Point(721, 583);
+            this.ShowAll.Name = "ShowAll";
+            this.ShowAll.Size = new System.Drawing.Size(176, 73);
+            this.ShowAll.TabIndex = 12;
+            this.ShowAll.Text = "SHOW ALL";
+            this.ShowAll.UseVisualStyleBackColor = false;
+            this.ShowAll.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(566, 408);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 39);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Brand";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(697, 416);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(307, 32);
+            this.textBox4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(1711, 1152);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ShowAll);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ShowAll;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ProjectDB/ProjectDB/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-404UC34G\\SQLEXPRESS;Initial Catalog=MyDbV2;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into UserTab values (@ID,@Name,@Price,@Brand)",con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Price", double.Parse(textBox3.Text));
+            cmd.Parameters.AddWithValue("@Brand", textBox4.Text);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Adaugat cu succes");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-404UC34G\\SQLEXPRESS;Initial Catalog=MyDbV2;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update UserTab set Name=@Name, Price=@Price where ID=@ID",con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Price", double.Parse(textBox3.Text));
+            cmd.Parameters.AddWithValue("@Brand", textBox4.Text);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Updateul a fost facut cu succes!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-404UC34G\\SQLEXPRESS;Initial Catalog=MyDbV2;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete UserTab where ID=@ID", con);
+            cmd.Parameters.AddWithValue("ID", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Sters cu succes");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-404UC34G\\SQLEXPRESS;Initial Catalog=MyDbV2;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from UserTab where ID=@ID", con);
+            cmd.Parameters.AddWithValue("ID", int.Parse(textBox1.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-404UC34G\\SQLEXPRESS;Initial Catalog=MyDbV2;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from UserTab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
